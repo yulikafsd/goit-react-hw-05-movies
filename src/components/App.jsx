b1f8f297f729925cdc909edf23bf6f58507aa7f0
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2023-01-06 15:33:16 +0100

    add routes in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<div>SharedLayout</div>}>
+          <Route index element={<div>Home</div>} />
+          <Route path="/movies" element={<div>Movies</div>} />
+          <Route path="/movies:movieId" element={<div>MovieDetails</div>}>
+            <Route path="cast" element={<div>MovieCast</div>} />
+            <Route path="reviews" element={<div>MovieReviews</div>} />
+          </Route>
+            <Route path="*" element={<div>Not Found</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+// https://api.themoviedb.org/3/movie/550?api_key=9ce583f6a8e715ddcf70f43e50ddfd1c
+// api_key=9ce583f6a8e715ddcf70f43e50ddfd1c
+// /trending/get-trending список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// /search/search-movies пошук фільму за ключовим словом на сторінці фільмів.
+// /movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
+// /movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
+// /movies/get-movie-reviews запит оглядів для сторінки кінофільму.
