b73a0ec8835a493e3d676a2e23a86999dab20783
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2023-01-06 17:15:25 +0100

    add reexport

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './SharedLayout/SharedLayout';
+export * from './Home/Home';
+export * from './Movies/Movies';
+export * from './MovieDetails/MovieDetails';
+export * from './Cast/Cast';
+export * from './Reviews/Reviews';
+export * from './NotFound/NotFound';
